3f9b0f51f40e808bc23c6824383077fd4957b05f
Antonin Hildebrand <antonin@hildebrand.cz>
2009-02-02 12:18:40 +0100

    window sliding is done using standard APIs, this should solve many headaches

--- VisorController.h
+++ VisorController.h
@@ -22,6 +22,7 @@
     NDHotKeyEvent *escapeKey;
     NSString* previouslyActiveApp;
     BOOL hidden;
+    float lastSlideOffset;
 }
 - (TermController *)controller;
 - (void)setController:(TermController *)value;
--- VisorController.m
+++ VisorController.m
@@ -6,14 +6,15 @@
 //  Copyright 2006 __MyCompanyName__. All rights reserved.
 //
 
+#define DEBUG_LOG_PATH "/Users/darwin/code/visor/Debug.log"
+
 #import "VisorController.h"
 #import "VisorWindow.h"
 #import "NDHotKeyEvent_QSMods.h"
 #import "VisorTermController.h"
 #import <QuartzComposer/QuartzComposer.h>
-#import "CGSPrivate.h"
 
-#define VisorTerminalDefaults @"VisorTerminal" 
+#define VisorTerminalDefaults @"VisorTerminal"
 
 NSString* stringForCharacter(const unsigned short aKeyCode, unichar aCharacter);
 
@@ -32,10 +33,24 @@ NSString* stringForCharacter(const unsigned short aKeyCode, unichar aCharacter);
     [VisorController sharedInstance];
 }
 
+// for SIMBL debugging
+// http://www.atomicbird.com/blog/2007/07/code-quickie-redirect-nslog
+- (void) redirectLog {
+    // set permissions for our NSLog file
+    NSBeep();
+    umask(022);
+    // send stderr to our file
+    FILE *newStderr = freopen(DEBUG_LOG_PATH, "w", stderr);
+}
+
 - (id) init {
     self = [super init];
     if (!self) return self;
 
+#ifdef _DEBUG_MODE
+    [self redirectLog];
+#endif
+    
     NSUserDefaults* ud = [NSUserDefaults standardUserDefaults];
     NSUserDefaultsController* udc = [NSUserDefaultsController sharedUserDefaultsController];
 
@@ -91,7 +106,9 @@ NSString* stringForCharacter(const unsigned short aKeyCode, unichar aCharacter);
     NSWindow *window=[controller window];
     [window setLevel:NSFloatingWindowLevel];
     [window setOpaque:NO];
-    [self placeWindowOffScreen:window];
+    [window setLevel:NSMainMenuWindowLevel-1];
+    [self resetWindowPlace:window];
+    [window setHasShadow:NO];
 
     [dnc addObserver:self selector:@selector(resignMain:) name:NSWindowDidResignMainNotification object:window];
     [dnc addObserver:self selector:@selector(resignKey:) name:NSWindowDidResignKeyNotification object:window];
@@ -146,35 +163,50 @@ NSString* stringForCharacter(const unsigned short aKeyCode, unichar aCharacter);
     }
 }
 
-- (void)placeWindowOffScreen:(id)window {
-    BOOL useBackground = [[NSUserDefaults standardUserDefaults]boolForKey:@"VisorUseBackgroundAnimation"];
-    NSScreen *screen=[NSScreen mainScreen];
+- (void)resetWindowPlace:(id)window {
+    float offset = 1.0f;
+    if (hidden) offset = 0.0f;
+    NSLog(@"offf %f", offset);
+    [self placeWindow:window offset:offset];
+    [self adoptScreenWidth:window];
+}
+
+// offset==0.0 means window is "hidden" above top screen edge
+// offset==1.0 means window is visible right under top screen edge
+- (void)placeWindow:(id)window offset:(float)offset {
+    NSScreen* screen=[NSScreen mainScreen];
     NSRect screenRect=[screen frame];
     screenRect.size.height-=21; // ignore menu area
-    NSRect showFrame=screenRect; // shown Frame
-    showFrame=[window frame]; // respect the existing height
-    showFrame.size.width=screenRect.size.width; // make it the full screen width
-    [window setFrame:showFrame display:NO];
-    showFrame.origin.x+=NSMidX(screenRect)-NSMidX(showFrame); // center horizontally
-    showFrame.origin.y=NSMaxY(screenRect); // move above top of screen
-    [window setFrame:showFrame display:NO];
+    NSRect frame=screenRect; // shown Frame
+    frame=[window frame]; // respect the existing height
+    frame.origin.y=NSMaxY(screenRect)-round(offset*NSHeight(frame)); // move above top of screen
+    [window setFrame:frame display:NO];
+    [self updateBackgroundFrame];
+}
+
+- (void)adoptScreenWidth:(id)window {
+    NSScreen* screen=[NSScreen mainScreen];
+    NSRect screenRect=[screen frame];
+    NSRect frame=screenRect; // shown Frame
+    frame=[window frame]; // respect the existing height
+    frame.size.width=screenRect.size.width; // make it the full screen width
+    frame.origin.x+=NSMidX(screenRect)-NSMidX(frame); // center horizontally
+    [window setFrame:frame display:NO];
+    [self updateBackgroundFrame];
+}
+
+- (void)updateBackgroundFrame {
+    BOOL useBackground = [[NSUserDefaults standardUserDefaults]boolForKey:@"VisorUseBackgroundAnimation"];
     if (useBackground) {
         [self backgroundWindow];
-        [[backgroundWindow contentView]startRendering];
-        [backgroundWindow setFrame:showFrame display:YES];
-        [backgroundWindow orderFront:nil];
-        [backgroundWindow setLevel:NSMainMenuWindowLevel-2];    
+        NSWindow* window = [controller window];
+        [backgroundWindow setFrame:[window frame] display:YES];
     } else {
         [self setBackgroundWindow:nil];
     }
-    [window setLevel:NSMainMenuWindowLevel-1];
 }
 
-- (void)showWindow {
-    if (!hidden) return;
-    hidden = false;
-    [self maybeEnableEscapeKey:YES];
-    
+- (void)storePreviouslyActiveApp {
     NSDictionary *activeAppDict = [[NSWorkspace sharedWorkspace] activeApplication];
     if (previouslyActiveApp) {
         [previouslyActiveApp release];
@@ -183,12 +215,33 @@ NSString* stringForCharacter(const unsigned short aKeyCode, unichar aCharacter);
     if ([[activeAppDict objectForKey:@"NSApplicationBundleIdentifier"] compare:@"com.apple.Terminal"]) {
         previouslyActiveApp = [[NSString alloc] initWithString:[activeAppDict objectForKey:@"NSApplicationPath"]];
     }
+}
+
+- (void)restorePreviouslyActiveApp {
+    if (!previouslyActiveApp) return;
+    NSDictionary *scriptError = [[NSDictionary alloc] init]; 
+    NSString *scriptSource = [NSString stringWithFormat: @"tell application \"%@\" to activate ", previouslyActiveApp]; 
+    NSAppleScript *appleScript = [[NSAppleScript alloc] initWithSource:scriptSource]; 
+    [appleScript executeAndReturnError: &scriptError];
+    [appleScript release];
+    [scriptError release];
+    [previouslyActiveApp release];
+    previouslyActiveApp = nil;
+}
+
+- (void)showWindow {
+    if (!hidden) return;
+    hidden = false;
+    [self storePreviouslyActiveApp];
+    [self maybeEnableEscapeKey:YES];
     [NSApp activateIgnoringOtherApps:YES];
-    NSWindow *window=[[self controller] window];
+    NSWindow *window=[controller window];
     [window makeKeyAndOrderFront:self];
     [window makeFirstResponder:[[controller selectedTabController] view]];
-    [self placeWindowOffScreen:window];
     [window setHasShadow:YES];
+    if (backgroundWindow) {
+        [[backgroundWindow contentView]startRendering];
+    }
     [self slideWindows:1];
     [window invalidateShadow];
 }
@@ -200,93 +253,63 @@ NSString* stringForCharacter(const unsigned short aKeyCode, unichar aCharacter);
     if (hidden) return;
     hidden = true;
     [self maybeEnableEscapeKey:NO];
-    if (previouslyActiveApp) {
-        NSDictionary *scriptError = [[NSDictionary alloc] init]; 
-        NSString *scriptSource = [NSString stringWithFormat: @"tell application \"%@\" to activate ", previouslyActiveApp]; 
-        NSAppleScript *appleScript = [[NSAppleScript alloc] initWithSource:scriptSource]; 
-        [appleScript executeAndReturnError: &scriptError];
-        [appleScript release];
-        [scriptError release];
-        [previouslyActiveApp release];
-        previouslyActiveApp = nil;
-    }
-    
+    [self restorePreviouslyActiveApp];
     NSWindow *window=[[self controller] window];
     NSScreen *screen=[NSScreen mainScreen];
     NSRect screenRect=[screen frame];   
     NSRect showFrame=screenRect; // Shown Frame
     NSRect hideFrame=NSOffsetRect(showFrame,0,NSHeight(screenRect)/2+22); //hidden frame for start of animation
-                                                                          ///   if ([[NSUserDefaults standardUserDefaults]boolForKey:@"VisorUseBackgroundAnimation"]
-                                                                          //    [window setLevel:NSFloatingWindowLevel];
     [self saveDefaults];
     [self slideWindows:0];
     [window setHasShadow:NO];
-    
-    [[backgroundWindow contentView]stopRendering];
+    [window invalidateShadow];
+    if (backgroundWindow) {
+        [[backgroundWindow contentView]stopRendering];
+    }
 }
 
-- (void)slideWindows:(BOOL)show {
-    NSAutoreleasePool *pool=[[NSAutoreleasePool alloc]init];
-    NSWindow *window=[[self controller] window];
-    float windowHeight=NSHeight([window frame]);
-    CGSConnection cgs = _CGSDefaultConnection();
-    int wids[2]={[window windowNumber],[backgroundWindow windowNumber]};
-    
-    CGAffineTransform transform;
-    CGSGetWindowTransform(cgs,wids[0],&transform);
-    
-    CGAffineTransform newTransforms[2];
-    float newAlphas[2]; 
-    NSTimeInterval t;
-    NSDate *date=[NSDate date];
-    int windowCount=backgroundWindow?2:1;
-    
-    // added drp
-    float DURATION=[[NSUserDefaults standardUserDefaults]floatForKey:@"VisorAnimationSpeed"];
-    
-    int windowHeightDelta = 500;
-    // added drp
-    // if we dont have to animate, dont really bother.
-    if(![[NSUserDefaults standardUserDefaults]boolForKey:@"VisorUseSlide"] && ![[NSUserDefaults standardUserDefaults]boolForKey:@"VisorUseFade"])
-        DURATION=0.1;
+#define SLIDE_EASING(x) sin(M_PI_2*(x))
+#define ALPHA_EASING(x) (1.0f-(x))
+#define SLIDE_DIRECTION(d,x) (d?(x):1.0f-(x))
+#define ALPHA_DIRECTION(d,x) (d?1.0f-(x):(x))
 
-    while (DURATION>(t=-[date timeIntervalSinceNow])) {
-        float f=t/DURATION;
-        
-        newTransforms[0]=newTransforms[1]=CGAffineTransformTranslate(transform,0,(show?-1:1)*sin(M_PI_2*f)*(windowHeight));
-        
-        // added drp do we fade in or not?
-        if ([[NSUserDefaults standardUserDefaults]boolForKey:@"VisorUseFade"])
-        {
-            if (backgroundWindow)
-                CGSSetWindowAlpha(cgs, wids[1], 1.0f-(f*1.1)); //background fades faster
-            CGSSetWindowAlpha(cgs, wids[0], 1.0f-f);
+- (void)slideWindows:(BOOL)direction { // true == down
+    NSAutoreleasePool* pool=[[NSAutoreleasePool alloc]init];
+    NSWindow* window=[controller window];
+
+    BOOL doSlide = [[NSUserDefaults standardUserDefaults]boolForKey:@"VisorUseSlide"];
+    BOOL doFade = [[NSUserDefaults standardUserDefaults]boolForKey:@"VisorUseFade"];
+    float animSpeed = [[NSUserDefaults standardUserDefaults]floatForKey:@"VisorAnimationSpeed"];
+
+    // animation loop
+    if (doFade || doSlide) {
+        if (!doSlide && direction) {
+            float offset = SLIDE_DIRECTION(direction, SLIDE_EASING(1));
+            [self placeWindow:window offset:offset];
         }
-        else
-        {
-            if (backgroundWindow)
-                CGSSetWindowAlpha(cgs, wids[1], 1.0f);
-            CGSSetWindowAlpha(cgs, wids[0], 1.0f);
-            
+        NSTimeInterval t;
+        NSDate* date=[NSDate date];
+        while (animSpeed>(t=-[date timeIntervalSinceNow])) {
+            float k=t/animSpeed;
+            if (doSlide) {
+                float offset = SLIDE_DIRECTION(direction, SLIDE_EASING(k));
+                [self placeWindow:window offset:offset];
+            }
+            if (doFade) {
+                float alpha = ALPHA_DIRECTION(direction, ALPHA_EASING(k));
+                if (backgroundWindow) [backgroundWindow setAlphaValue:alpha];
+                [window setAlphaValue:alpha];
+            }
+            usleep(5000); // 5ms
         }
-        
-        // added drp - do we animate the slide?
-        if ([[NSUserDefaults standardUserDefaults]boolForKey:@"VisorUseSlide"])
-        {
-            CGSSetWindowTransforms(cgs, wids, newTransforms, windowCount); 
-        }
-        
-        //[backgroundWindow display];
-        usleep(5000);
     }
-    
-    
-    newTransforms[0]=newTransforms[1]=CGAffineTransformTranslate(transform,0,(show?-1:1)*(windowHeight));
-    CGSSetWindowTransforms(cgs, wids, newTransforms, windowCount); 
-    CGSSetWindowAlpha(cgs, wids[1], 1);
-    CGSSetWindowAlpha(cgs, wids[0], 1);
-    [window setAlphaValue:1.0];  // NSWindow caches these values, so let it know
-    [backgroundWindow setAlphaValue:1.0];
+
+    // apply final state
+    float offset = SLIDE_DIRECTION(direction, SLIDE_EASING(1));
+    [self placeWindow:window offset:offset];
+    // always set final alpha to 1, for case off-screen window gets onto the screen somehow (imagine display resize?)
+    if (backgroundWindow) [backgroundWindow setAlphaValue:1.0]; // NSWindow caches these values, so let it know
+    [window setAlphaValue:1.0]; // NSWindow caches these values, so let it know
 }
 
 // Callback for a closed shell
@@ -320,11 +343,6 @@ NSString* stringForCharacter(const unsigned short aKeyCode, unichar aCharacter);
     }
 }
 
-- (void)windowResized {
-    [backgroundWindow setFrame:[[controller window]frame] display:YES]; 
-    [self saveDefaults];
-}
-
 - (IBAction)chooseFile:(id)sender {
     NSOpenPanel *panel=[NSOpenPanel openPanel];
     [panel setTitle:@"Select a Quartz Composer (qtz) file"];
@@ -336,15 +354,22 @@ NSString* stringForCharacter(const unsigned short aKeyCode, unichar aCharacter);
     }
 }
 
+- (void)windowResized {
+    [self adoptScreenWidth:[controller window]];
+    [self saveDefaults];
+}
+
 - (void)resized:(NSNotification *)notif {
-    if (!backgroundWindow) return;
-    [backgroundWindow setFrame:[[controller window] frame] display:YES]; 
+    [self adoptScreenWidth:[controller window]];
+    [self saveDefaults];
 }
 
 - (NSWindow *)backgroundWindow {
     if (backgroundWindow) return [[backgroundWindow retain] autorelease];
 
     backgroundWindow = [[[NSWindow class] alloc] initWithContentRect:NSZeroRect styleMask:NSBorderlessWindowMask backing:NSBackingStoreBuffered defer:NO];
+    [backgroundWindow orderFront:nil];
+    [backgroundWindow setLevel:NSMainMenuWindowLevel-2];    
     [backgroundWindow setIgnoresMouseEvents:YES];
     [backgroundWindow setBackgroundColor: [NSColor blueColor]];
     [backgroundWindow setOpaque:NO];
