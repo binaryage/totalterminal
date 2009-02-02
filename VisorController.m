cb0a043339a34f308f02385d0356df15c705723a
Antonin Hildebrand <antonin@hildebrand.cz>
2009-02-02 07:50:58 +0100

    refactoring

--- VisorController.m
+++ VisorController.m
@@ -49,81 +49,78 @@ NSString* stringForCharacter( const unsigned short aKeyCode, unichar aCharacter 
 
 + (void) install {
     NSDictionary *defaults=[NSDictionary dictionaryWithContentsOfFile:[[NSBundle bundleForClass:[self class]]pathForResource:@"Defaults" ofType:@"plist"]];
-    //NSLog(@"defaults %@",defaults);
     [[NSUserDefaults standardUserDefaults]registerDefaults:defaults];
     [VisorController sharedInstance];
 }
 
 - (id) init {
     self = [super init];
+    if (!self) return self;
+
+    NSUserDefaults* ud = [NSUserDefaults standardUserDefaults];
+    NSUserDefaultsController* udc = [NSUserDefaultsController sharedUserDefaultsController];
+
     previouslyActiveApp = nil;
     hidden = true;
-    if (self != nil) {
+
     NSDictionary *defaults=[NSDictionary dictionaryWithContentsOfFile:[[NSBundle bundleForClass:[self class]]pathForResource:@"Defaults" ofType:@"plist"]];
-    //NSLog(@"defaults %@",defaults);
-    [[NSUserDefaults standardUserDefaults]registerDefaults:defaults];
+    [ud registerDefaults:defaults];
     
-        hotkey=nil;
-        [NSBundle loadNibNamed:@"Visor" owner:self];
+    hotkey=nil;
+    [NSBundle loadNibNamed:@"Visor" owner:self];
 
-        //if the default VisorShowStatusItem doesn't exist, set it to true by default
-        if (![[NSUserDefaults standardUserDefaults] objectForKey:@"VisorShowStatusItem"]) {
-            [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"VisorShowStatusItem"];
-        }
-        
-        //add the "Visor Preferences..." item to the Terminal menu
-        id <NSMenuItem> prefsMenuItem = [[statusMenu itemAtIndex:[statusMenu numberOfItems] - 1] copy];
-        [[[[NSApp mainMenu] itemAtIndex:0] submenu] insertItem:prefsMenuItem atIndex:3];
-        [prefsMenuItem release];
-        
-        if ([[NSUserDefaults standardUserDefaults] boolForKey:@"VisorShowStatusItem"]) {
-            [self activateStatusMenu];
-        }
-        
-
-        
-        [self enableHotKey];
-        [self initEscapeKey];
-        
-        // Watch for hotkey changes
-        [[NSUserDefaultsController sharedUserDefaultsController]addObserver:self
-                                                                 forKeyPath:@"values.VisorHotKey"
-                                                                    options:nil
-                                                                    context:nil];
-        
-        [[NSUserDefaultsController sharedUserDefaultsController]addObserver:self
-                                                                 forKeyPath:@"values.VisorBackgroundAnimationFile"
-                                                                    options:nil
-                                                                    context:nil];
-        // added drp
-        
-        [[NSUserDefaultsController sharedUserDefaultsController]addObserver:self
-                                                                 forKeyPath:@"values.VisorUseFade"
-                                                                    options:nil
-                                                                    context:nil];                                                           
-        
-        
-        [[NSUserDefaultsController sharedUserDefaultsController]addObserver:self
-                                                                 forKeyPath:@"values.VisorUseSlide"
-                                                                    options:nil
-                                                                    context:nil];               
-        
-        [[NSUserDefaultsController sharedUserDefaultsController]addObserver:self
-                                                                 forKeyPath:@"values.VisorAnimationSpeed"
-                                                                    options:nil
-                                                                    context:nil];
-        
-        [[NSUserDefaultsController sharedUserDefaultsController]addObserver:self
-                                                                 forKeyPath:@"values.VisorShowStatusItem"
-                                                                    options:nil
-                                                                    context:nil];
-        
-        NSWindow *window=[[self controller] window];
-        if ([[NSUserDefaults standardUserDefaults]boolForKey:@"VisorUseBackgroundAnimation"]){
-            [self backgroundWindow];
-        }
+    // if the default VisorShowStatusItem doesn't exist, set it to true by default
+    if (![ud objectForKey:@"VisorShowStatusItem"]) {
+        [ud setBool:YES forKey:@"VisorShowStatusItem"];
+    }
+    
+    // add the "Visor Preferences..." item to the Terminal menu
+    id <NSMenuItem> prefsMenuItem = [[statusMenu itemAtIndex:[statusMenu numberOfItems] - 1] copy];
+    [[[[NSApp mainMenu] itemAtIndex:0] submenu] insertItem:prefsMenuItem atIndex:3];
+    [prefsMenuItem release];
+    
+    if ([ud  boolForKey:@"VisorShowStatusItem"]) {
+        [self activateStatusMenu];
+    }
+    
+    [self enableHotKey];
+    [self initEscapeKey];
+    
+    // watch for hotkey changes
+    [udc addObserver:self forKeyPath:@"values.VisorHotKey" options:nil context:nil];
+    [udc addObserver:self forKeyPath:@"values.VisorBackgroundAnimationFile" options:nil context:nil];
+    [udc addObserver:self forKeyPath:@"values.VisorUseFade" options:nil context:nil];                                                           
+    [udc addObserver:self forKeyPath:@"values.VisorUseSlide" options:nil context:nil];               
+    [udc addObserver:self forKeyPath:@"values.VisorAnimationSpeed" options:nil context:nil];
+    [udc addObserver:self forKeyPath:@"values.VisorShowStatusItem" options:nil context:nil];
+    
+    if ([ud boolForKey:@"VisorUseBackgroundAnimation"]) {
+        [self backgroundWindow];
     }
     return self;
+}
+
+- (void)createController {
+    if (controller) return;
+    id profile = [[TTProfileManager sharedProfileManager] profileWithName:@"Visor"];
+    NSNotificationCenter* dnc = [NSNotificationCenter defaultCenter];
+
+    NSDisableScreenUpdates();
+    controller = [NSApp newWindowControllerWithProfile:profile];
+    [[controller window] orderOut:nil];
+    NSEnableScreenUpdates();
+
+    NSWindow *window=[controller window];
+    [window setDelegate:controller];
+    [window setLevel:NSFloatingWindowLevel];
+    [window setOpaque:NO];
+    [window setBackgroundColor:[NSColor lightGrayColor]];
+    [controller setWindow:window];
+    [dnc addObserver:self selector:@selector(resignMain:) name:NSWindowDidResignMainNotification object:window];
+    [dnc addObserver:self selector:@selector(resignKey:) name:NSWindowDidResignKeyNotification object:window];
+    [dnc addObserver:self selector:@selector(becomeKey:) name:NSWindowDidBecomeKeyNotification object:window];
+    [dnc addObserver:self selector:@selector(resized:) name:NSWindowDidResizeNotification object:window];
+    [window setOpaque:NO];
 }
 
 - (BOOL)validateMenuItem:(id <NSMenuItem>)menuItem {
@@ -172,6 +169,31 @@ NSString* stringForCharacter( const unsigned short aKeyCode, unichar aCharacter 
     }
 }
 
+- (void)placeWindowOffScreen {
+    BOOL useBackground = [[NSUserDefaults standardUserDefaults]boolForKey:@"VisorUseBackgroundAnimation"];
+    NSScreen *screen=[NSScreen mainScreen];
+    NSWindow *window=[[self controller] window];
+    NSRect screenRect=[screen frame];
+    screenRect.size.height-=21; // ignore menu area
+    NSRect showFrame=screenRect; // shown Frame
+    showFrame=[window frame]; // respect the existing height
+    showFrame.size.width=screenRect.size.width; // make it the full screen width
+    [window setFrame:showFrame display:NO];
+    showFrame.origin.x+=NSMidX(screenRect)-NSMidX(showFrame); // center horizontally
+    showFrame.origin.y=NSMaxY(screenRect); // move above top of screen
+    [window setFrame:showFrame display:NO];
+    if (useBackground) {
+        [self backgroundWindow];
+        [[backgroundWindow contentView]startRendering];
+        [backgroundWindow setFrame:showFrame display:YES];
+        [backgroundWindow orderFront:nil];
+        [backgroundWindow setLevel:NSMainMenuWindowLevel-2];    
+    } else {
+        [self setBackgroundWindow:nil];
+    }
+    [window setLevel:NSMainMenuWindowLevel-1];
+}
+
 - (void)showWindow {
     if (!hidden) return;
     hidden = false;
@@ -186,34 +208,10 @@ NSString* stringForCharacter( const unsigned short aKeyCode, unichar aCharacter 
         previouslyActiveApp = [[NSString alloc] initWithString:[activeAppDict objectForKey:@"NSApplicationPath"]];
     }
     [NSApp activateIgnoringOtherApps:YES];
-  
-    NSScreen *screen=[NSScreen mainScreen];
-    NSRect screenRect=[screen frame];
-    screenRect.size.height-=21; // Ignore menu area
-    
     NSWindow *window=[[self controller] window];
-    NSRect showFrame=screenRect; // Shown Frame
-    showFrame=[window frame]; // respect the existing height
-    showFrame.size.width=screenRect.size.width;//make it the full screen width
-    [window setFrame:showFrame display:NO];
-    showFrame=[[controller window]frame];
-    showFrame.origin.x+=NSMidX(screenRect)-NSMidX(showFrame); // center horizontally
-    showFrame.origin.y=NSMaxY(screenRect); //-NSHeight(showFrame); // align to top of screen
     [window makeKeyAndOrderFront:self];
-    [window setFrame:showFrame display:NO];
-    if ([[NSUserDefaults standardUserDefaults]boolForKey:@"VisorUseBackgroundAnimation"]){
-        [self backgroundWindow];
-        [[backgroundWindow contentView]startRendering];
-        [backgroundWindow setFrame:showFrame display:YES];
-        //[backgroundWindow setAlphaValue:0.0];
-        [backgroundWindow orderFront:nil];
-        [backgroundWindow setLevel:NSMainMenuWindowLevel-2];    
-    }else{
-        [self setBackgroundWindow:nil];
-    }
-        
-    [window setLevel:NSMainMenuWindowLevel-1];
     [window makeFirstResponder:[[controller selectedTabController] view]];
+    [self placeWindowOffScreen];
     [window setHasShadow:YES];
     [self slideWindows:1];
     [window invalidateShadow];
@@ -351,56 +349,6 @@ NSString* stringForCharacter( const unsigned short aKeyCode, unichar aCharacter 
     [self saveDefaults];
 }
 
-- (void)createController {
-    if (!controller){     
-        id profile = [[TTProfileManager sharedProfileManager] profileWithName:@"Visor"];
-
-        BOOL sizeWindow=NO;
-
-        NSDisableScreenUpdates();
-        controller = [NSApp newWindowControllerWithProfile:profile];
-        [[controller window] orderOut:nil];
-        NSEnableScreenUpdates();
-
-        NSWindow *win=[controller window];
-        [win setDelegate:controller];
-        [win setLevel:NSFloatingWindowLevel];
-        [win setOpaque:NO];
-        [win setBackgroundColor:[NSColor lightGrayColor]];
-        [controller setWindow:win];
-        [[NSNotificationCenter defaultCenter]addObserver:self
-                                                selector:@selector(resignMain:)
-                                                    name:NSWindowDidResignMainNotification
-                                                  object:win];
-        [[NSNotificationCenter defaultCenter]addObserver:self
-                                                selector:@selector(resignKey:)
-                                                    name:NSWindowDidResignKeyNotification
-                                                  object:win];
-        [[NSNotificationCenter defaultCenter]addObserver:self
-                                            selector:@selector(becomeKey:)
-                                                name:NSWindowDidBecomeKeyNotification
-                                              object:win];
-        [[NSNotificationCenter defaultCenter]addObserver:self
-                                                selector:@selector(resized:)
-                                                    name:NSWindowDidResizeNotification
-                                                  object:win];
-    
-        [win setOpaque:NO];
-        
-        if (sizeWindow){
-            NSScreen *screen=[NSScreen mainScreen];
-            NSRect screenRect=[screen frame];
-            screenRect.size.height-=22; // Ignore menu area
-        
-            NSWindow *window=[[self controller] window];
-            NSRect showFrame=screenRect; // Shown Frame
-            showFrame.origin.y+=NSHeight(screenRect)/2;
-            showFrame.size.height=NSHeight(screenRect)/2;
-            [win setFrame:showFrame display:NO];
-        }
-    }
-}
-
 - (IBAction)chooseFile:(id)sender {
     NSOpenPanel *panel=[NSOpenPanel openPanel];
     [panel setTitle:@"Select a Quartz Composer (qtz) file"];
@@ -413,43 +361,41 @@ NSString* stringForCharacter( const unsigned short aKeyCode, unichar aCharacter 
 }
 
 - (void)resized:(NSNotification *)notif {
-    if (backgroundWindow) {
-        [backgroundWindow setFrame:[[controller window] frame] display:YES]; 
-    }
+    if (!backgroundWindow) return;
+    [backgroundWindow setFrame:[[controller window] frame] display:YES]; 
 }
 
 - (NSWindow *)backgroundWindow {
-    if (!backgroundWindow){
-        backgroundWindow = [[[NSWindow class] alloc] initWithContentRect:NSZeroRect styleMask:NSBorderlessWindowMask backing:NSBackingStoreBuffered defer:NO];
-        [backgroundWindow setIgnoresMouseEvents:YES];
-        [backgroundWindow setBackgroundColor: [NSColor blueColor]];
-        [backgroundWindow setOpaque:NO];
-        [backgroundWindow setHasShadow:NO];
-        [backgroundWindow setReleasedWhenClosed:YES];
-        [backgroundWindow setLevel:NSFloatingWindowLevel];
-        [backgroundWindow setHasShadow:NO];
-        QCView *content=[[[QCView alloc]init]autorelease];
-        
-        [backgroundWindow setContentView:content];
-        
-        
-        
-        NSString *path=[[NSUserDefaults standardUserDefaults]stringForKey:@"VisorBackgroundAnimationFile"];
-        path=[path stringByStandardizingPath];
-        
-        NSFileManager *fm=[NSFileManager defaultManager];
-        if (![fm fileExistsAtPath:path]){
-            NSLog(@"animation does not exist: %@",path);
-            path=nil;
-        }
-        
-        if (!path)
-            path=[[NSBundle bundleForClass:[self class]]pathForResource:@"Visor" ofType:@"qtz"];
-        
-        [content loadCompositionFromFile:path];
-        [content startRendering];
-        [content setMaxRenderingFrameRate:15.0];
+    if (backgroundWindow) return [[backgroundWindow retain] autorelease];
+
+    backgroundWindow = [[[NSWindow class] alloc] initWithContentRect:NSZeroRect styleMask:NSBorderlessWindowMask backing:NSBackingStoreBuffered defer:NO];
+    [backgroundWindow setIgnoresMouseEvents:YES];
+    [backgroundWindow setBackgroundColor: [NSColor blueColor]];
+    [backgroundWindow setOpaque:NO];
+    [backgroundWindow setHasShadow:NO];
+    [backgroundWindow setReleasedWhenClosed:YES];
+    [backgroundWindow setLevel:NSFloatingWindowLevel];
+    [backgroundWindow setHasShadow:NO];
+    QCView *content=[[[QCView alloc]init]autorelease];
+    
+    [backgroundWindow setContentView:content];
+    
+    NSString *path=[[NSUserDefaults standardUserDefaults]stringForKey:@"VisorBackgroundAnimationFile"];
+    path=[path stringByStandardizingPath];
+    
+    NSFileManager *fm=[NSFileManager defaultManager];
+    if (![fm fileExistsAtPath:path]){
+        NSLog(@"animation does not exist: %@",path);
+        path=nil;
     }
+    
+    if (!path)
+        path=[[NSBundle bundleForClass:[self class]]pathForResource:@"Visor" ofType:@"qtz"];
+    
+    [content loadCompositionFromFile:path];
+    [content startRendering];
+    [content setMaxRenderingFrameRate:15.0];
+
     return [[backgroundWindow retain] autorelease];
 }
 
@@ -502,21 +448,14 @@ NSString* stringForCharacter( const unsigned short aKeyCode, unichar aCharacter 
 }
 
 - (TermController *)controller {
-    
     if (!controller)[self createController];
-    
     return [[controller retain] autorelease];
 }
 
 - (void)setController:(TermController *)value {
-    if (controller != value) {
-        [controller release];
-        controller = [value retain];
-    }
-}
-
-- (IBAction)setHotKey:(id)sender{
-    NSLog(@"sender %@",sender); 
+    if (controller==value) return;
+    [controller release];
+    controller = [value retain];
 }
 
 @end
